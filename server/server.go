10a839c247a29510f19d539cefad991655c9870a
Louis Falk Knudsen <louis@falkknudsen.com>
2025-10-23 14:02:03 +0200

    Messages which the server receives from clients are broadcast to all clients.
    Co-authored-by: Vitus <vitusbrodersen@gmail.com>
    Co-authored-by: Kris <krme@itu.dk>

--- server/server.go
+++ server/server.go
@@ -10,18 +10,23 @@ import (
 	"google.golang.org/grpc"
 )
 
+const VERBOSE = false
+
 type ChitChatService struct {
 	UnimplementedChitChatServer
 	connections []Connection
+	channel     chan *Message
 }
 
 type Connection struct {
-	conn    grpc.BidiStreamingServer[Message, Message]
-	channel chan string
+	conn           grpc.BidiStreamingServer[Message, Message]
+	homeChannel    chan *Message
+	receiveChannel chan *Message
 }
 
 func (s *ChitChatService) RouteChat(server grpc.BidiStreamingServer[Message, Message]) error {
-	conn := Connection{server, make(chan string)}
+	log.Println("Established connection with a new client.")
+	conn := Connection{server, s.channel, make(chan *Message)}
 	s.connections = append(s.connections, conn)
 	go conn.Listen()
 	for {
@@ -30,12 +35,48 @@ func (s *ChitChatService) RouteChat(server grpc.BidiStreamingServer[Message, Mes
 }
 
 func (c *Connection) Listen() {
+	go func() {
+		if VERBOSE {
+			log.Println("Listening to channels.")
+		}
+		for {
+			msg := <-c.receiveChannel
+			err := c.conn.Send(msg)
+			if err != nil {
+				log.Fatalf("fail to send message: %v", err)
+			}
+		}
+	}()
+	if VERBOSE {
+		log.Println("Listening to stream.")
+	}
 	for {
 		recv, err := c.conn.Recv()
 		if err != nil {
 			return
 		}
-		log.Print("Msg: " + recv.GetText())
+		if VERBOSE {
+			log.Printf("Received message: %s", recv)
+		}
+		c.homeChannel <- recv
+		// log.Printf("Conveyed message to home: %s", recv)
+	}
+}
+
+func (s *ChitChatService) ManageChannels() {
+	if VERBOSE {
+		log.Println("Managing channels!")
+	}
+	for {
+		select {
+		case msg := <-s.channel:
+			if VERBOSE {
+				log.Println("Got message from home channel:" + msg.GetText())
+			}
+			for _, conn := range s.connections {
+				conn.receiveChannel <- msg
+			}
+		}
 	}
 }
 
@@ -47,8 +88,12 @@ func main() {
 	fmt.Printf("Listening on %v\n", lis.Addr())
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
-	service := ChitChatService{}
+	service := ChitChatService{
+		connections: []Connection{},
+		channel:     make(chan *Message),
+	}
 	RegisterChitChatServer(grpcServer, &service)
+	go service.ManageChannels()
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Fatalf("failed to serve: %v", err)
