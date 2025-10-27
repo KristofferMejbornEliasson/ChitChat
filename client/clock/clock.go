5f2e19d244499f796e50cb3c9fab0750d67d0cc2
Louis <louis@falkknudsen.com>
2025-10-28 00:19:23 +0100

    Fixed deadlock in Clock during increment.

--- client/clock/clock.go
+++ client/clock/clock.go
@@ -28,41 +28,64 @@ func (c *Clock) CopyVector() (copiedVector []int64) {
 	return copiedVector
 }
 
+func ensureArrayBigEnough(array *[]int64, index int32) (bigEnoughArray *[]int64) {
+	if array == nil {
+		arr := make([]int64, index+1)
+		array = &arr
+	}
+	if len(*array) <= int(index) {
+		arr := make([]int64, index+1)
+		copy(arr, *array)
+		array = &arr
+	}
+	return array
+}
+
 func (c *Clock) Increment(index int32) {
 	c.lock.Lock()
-	defer c.lock.Unlock()
+	ensureArrayBigEnough(&c.vector, index)
 	c.vector[index] += 1
+	c.lock.Unlock()
 }
 
 func (c *Clock) IncrementAndCopy(index int32) (updatedCopy []int64) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
+	ensureArrayBigEnough(&c.vector, index)
 	c.vector[index] += 1
 	updatedCopy = make([]int64, len(c.vector))
 	copy(updatedCopy, c.vector)
 	return updatedCopy
 }
 
-func (c *Clock) Update(vector []int64) {
+func (c *Clock) Update(other []int64) {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 	if c.vector == nil {
-		c.vector = vector
+		c.vector = other
 		return
 	}
-	if len(c.vector) <= len(vector) {
-		for i := 0; i < len(c.vector); i++ {
-			if c.vector[i] < vector[i] {
-				c.vector[i] = vector[i]
+	if other == nil {
+		return
+	}
+
+	if len(c.vector) < len(other) { // If other array is longer, we update *it*, then copy it into ourselves.
+		for i := 0; i < len(other); i++ {
+			if c.vector[i] < other[i] {
+				other[i] = c.vector[i]
 			}
 		}
-		for len(c.vector) < len(vector) {
-			c.vector = append(c.vector, vector[len(c.vector)-1])
+		c.vector = other
+	} else if len(c.vector) == len(other) {
+		for i := 0; i < len(c.vector); i++ {
+			if c.vector[i] < other[i] {
+				c.vector[i] = other[i]
+			}
 		}
-	} else {
-		for i := 0; i < len(vector); i++ {
-			if c.vector[i] < vector[i] {
-				c.vector[i] = vector[i]
+	} else { // If other array is shorter, we update the elements we can
+		for i := 0; i < len(other); i++ {
+			if c.vector[i] < other[i] {
+				c.vector[i] = other[i]
 			}
 		}
 	}
