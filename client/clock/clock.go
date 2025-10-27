25b045a0bdf64d9e65cd7353331eb3eff26dc235
Louis <louis@falkknudsen.com>
2025-10-27 23:28:56 +0100

    Gave Clock some more exported methods so that incrementing, updating, and copying can be done during the same lock.

--- client/clock/clock.go
+++ client/clock/clock.go
@@ -1,6 +1,9 @@
 package clock
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+)
 
 type Clock struct {
 	vector []int64
@@ -11,17 +14,40 @@ func NewClock(vector []int64) *Clock {
 	return &Clock{vector: vector}
 }
 
+func (c *Clock) String() string {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return fmt.Sprint(c.vector)
+}
+
+func (c *Clock) CopyVector() (copiedVector []int64) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	copiedVector = make([]int64, len(c.vector))
+	copy(copiedVector, c.vector)
+	return copiedVector
+}
+
 func (c *Clock) Increment(index int32) {
 	c.lock.Lock()
+	defer c.lock.Unlock()
 	c.vector[index] += 1
-	c.lock.Unlock()
+}
+
+func (c *Clock) IncrementAndCopy(index int32) (updatedCopy []int64) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	c.vector[index] += 1
+	updatedCopy = make([]int64, len(c.vector))
+	copy(updatedCopy, c.vector)
+	return updatedCopy
 }
 
 func (c *Clock) Update(vector []int64) {
 	c.lock.Lock()
+	defer c.lock.Unlock()
 	if c.vector == nil {
 		c.vector = vector
-		c.lock.Unlock()
 		return
 	}
 	if len(c.vector) <= len(vector) {
@@ -40,5 +66,4 @@ func (c *Clock) Update(vector []int64) {
 			}
 		}
 	}
-	c.lock.Unlock()
 }
