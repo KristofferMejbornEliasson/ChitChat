222d2607a919ea86460cb351b28800c0db75afb4
Louis Falk Knudsen <louis@falkknudsen.com>
2025-10-28 16:54:17 +0100

    Client has logger too, now. The output file is in the temp directory.

--- client/client/client.go
+++ client/client/client.go
@@ -17,6 +17,7 @@ type Client struct {
 	id          int32
 	vectorClock *Clock
 	stream      grpc.BidiStreamingClient[Message, Message]
+	logger      *log.Logger
 }
 
 func (c *Client) IncrementClock() {
@@ -27,17 +28,28 @@ func (c *Client) IncrementAndCopyClock() (updatedCopy []int64) {
 	return c.vectorClock.IncrementAndCopy(c.id)
 }
 
-func NewClient(stream grpc.BidiStreamingClient[Message, Message]) *Client {
+func NewClient(stream grpc.BidiStreamingClient[Message, Message], logger *log.Logger) *Client {
 	in, err := stream.Recv()
 	if err != nil {
-		log.Fatalf("No initial response received from server:\n%v", err)
+		logger.Fatalf("No initial response received from server:\n%v", err)
 	}
-
-	return &Client{
+	client := Client{
 		id:          in.GetId(),
 		vectorClock: NewClock(in.GetClock()),
 		stream:      stream,
+		logger:      logger,
 	}
+	client.Log("Established connection with server.\n")
+	return &client
+}
+
+func (c *Client) Log(message string) {
+	c.logger.SetPrefix(c.vectorClock.String() + ": ")
+	c.logger.Print(message)
+}
+
+func (c *Client) Logf(format string, v ...any) {
+	c.Log(fmt.Sprintf(format, v...))
 }
 
 func (c *Client) PrintMessage(msg *Message) {
@@ -50,7 +62,8 @@ func (c *Client) Run() {
 		for {
 			in, err := c.stream.Recv()
 			if err != nil {
-				log.Fatalf("Connection was closed.")
+				wait <- struct{}{}
+				break
 			}
 			c.IncrementClock()
 			c.vectorClock.Update(in.GetClock())
@@ -58,32 +71,39 @@ func (c *Client) Run() {
 		}
 	}()
 	reader := bufio.NewScanner(os.Stdin)
-	for {
-		reader.Scan()
-		if reader.Err() != nil {
-			log.Fatalf("fail to call Read: %v", reader.Err())
-		}
-		text := reader.Text()
-		if text == "exit" {
-			close(wait)
-			break
-		}
-		if text == "" {
-			continue
-		}
+	go func() {
+		for {
+			reader.Scan()
+			if reader.Err() != nil {
+				log.Fatalf("fail to call Read: %v", reader.Err())
+			}
+			text := reader.Text()
+			if text == "exit" {
+				wait <- struct{}{}
+				break
+			}
+			if text == "" {
+				continue
+			}
 
-		err := c.stream.Send(&Message{
-			Text:  &text,
-			Id:    &c.id,
-			Clock: c.IncrementAndCopyClock(),
-		})
-		if VERBOSE {
-			fmt.Printf("Client #%d sent a message through its stream.\n", c.id)
+			err := c.stream.Send(&Message{
+				Text:  &text,
+				Id:    &c.id,
+				Clock: c.IncrementAndCopyClock(),
+			})
+			if VERBOSE {
+				fmt.Printf("Client #%d sent a message through its stream.\n", c.id)
+			}
+			if err != nil {
+				log.Fatalf("fail to call Send: %v", err)
+			}
 		}
-		if err != nil {
-			log.Fatalf("fail to call Send: %v", err)
+	}()
+	for {
+		select {
+		case <-wait:
+			_ = c.stream.CloseSend()
+			return
 		}
 	}
-	_ = c.stream.CloseSend()
-	<-wait
 }
--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -12,35 +13,48 @@ import (
 )
 
 func main() {
+	file, err := os.CreateTemp("", "client-*.txt")
+	logger := log.New(file, "", 0)
+	var c *Client
+	defer func(c *Client) {
+		if c != nil {
+			c.Log("Shutting down client.")
+		} else {
+			logger.Print("Shutting down client")
+		}
+		_ = file.Close()
+	}(c)
+
 	// Create a gRPC channel
 	// If credentials are needed, these are set in the options.
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err := grpc.NewClient("localhost:8080", opts...)
 	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
+		logger.Fatalf("Failed to dial: %v", err)
 	}
 	defer func(conn *grpc.ClientConn) {
-		err := conn.Close()
+		err = conn.Close()
 		if err != nil {
-			log.Fatalf("error closing connection:\n%v", err)
+			logger.Fatalf("Error closing connection:\n%v", err)
 		}
 	}(conn)
 
 	client := NewChitChatClient(conn)
 	stream, err := client.RouteChat(context.Background())
 	if err != nil {
-		log.Fatalf("fail to call RouteChat: %v", err)
+		logger.Fatalf("Failed to create a stream via call to RouteChat: %v", err)
 	}
 
-	txt := "This is a message from a new client! :)"
-	msg := &Message{}
-	msg.Text = &txt
+	txt := "Establishing connection."
+	msg := &Message{
+		Text: &txt,
+	}
 	err = stream.Send(msg)
 	if err != nil {
-		log.Fatalf("fail to call Send: %v", err)
+		logger.Fatalf("Failed to send establishing message: %v", err)
 	}
 
-	c := NewClient(stream)
+	c = NewClient(stream, logger)
 	c.Run()
 }
